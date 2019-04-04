557ead24216fcdb76f4699da2dcd0cf39efe2c6b
patrick-doucet <patrick.doucet6@gmail.com>
2019-04-04 20:01:11 -0300

    Adding graph class, adding simulation file, adding more methods to node class

--- classes/node.py
+++ classes/node.py
@@ -1,4 +1,7 @@
+import math
+
 class Node:
+
     def __init__(self, position, listenRange):
         
         # User defined parameters
@@ -6,14 +9,40 @@ class Node:
         self.listenRange = listenRange
 
         # Default init parameters
+        self.nodeList = []
         self.arrivalPacket = []
         self.arrivalTime = []
         self.arrivalSize = []
         self.state = -1
-        
-        
 
-n = Node({2, 3}, 10)
+        # Trickle parameters
+        self.Imin = -1 # Minimum length of interval
+        self.Imax = -1 # Minimum length of interval
+        self.k = -1 # Redundancy k
+        self.I = -1 # Length of current interval
+        self.t = -1 # Random time t inside [I/2, I]
+        self.c = -1 # Consistant message counter
 
-print(n.position)
-print(n.listenRange)
+    # Simulation methods
+    def get_node_list(self):
+        return nodeList
+    
+    def update_node_list(self):
+        return
+
+    def distance_between_2_points(self, point1, point2):
+        return math.sqrt(math.pow(point2['x'] - point1['x'], 2) + math.pow(point2['y'] - point1['y'], 2))
+
+    def is_in_range(self, point1, point2):
+        return self.distance_between_2_points(point1, point2) < self.listenRange
+
+    # Trickle methods
+    def listen(self):
+        return
+
+    def update(self, state):
+        return
+
+    def transmit(self, nodeList):
+        return
+    
