aad4e54d2f646224a4867d45bce18e557ff0d3e1
jflp <ejl6048@umoncton.ca>
2019-04-05 20:43:55 -0300

    Adding dynamic scaling to graphics

--- simulation.py
+++ simulation.py
@@ -9,10 +9,10 @@ nC = node.Node(g, 'C', {'x': 2, 'y': 5}, 3)
 nD = node.Node(g, 'D', {'x': 5, 'y': 10}, 4)
 nE = node.Node(g, 'E', {'x': 0, 'y': 0}, 3)
 nF = node.Node(g, 'F', {'x': 3, 'y': 8}, 3)
-nG = node.Node(g, 'G', {'x': 4, 'y': 8}, 3)
-nH = node.Node(g, 'H', {'x': 3, 'y': 7}, 4)
-nI = node.Node(g, 'I', {'x': 2, 'y': 6}, 3)
-nJ = node.Node(g, 'J', {'x': 3, 'y': 5}, 3)
+#nG = node.Node(g, 'G', {'x': 40, 'y': 80}, 30)
+#nH = node.Node(g, 'H', {'x': 30, 'y': 70}, 40)
+#nI = node.Node(g, 'I', {'x': 20, 'y': 60}, 30)
+#nJ = node.Node(g, 'J', {'x': 30, 'y': 50}, 30)
 
 # Adding nodes to graph
 g.add_node(nA)
@@ -21,10 +21,10 @@ g.add_node(nC)
 g.add_node(nD)
 g.add_node(nE)
 g.add_node(nF)
-g.add_node(nG)
-g.add_node(nH)
-g.add_node(nI)
-g.add_node(nJ)
+#g.add_node(nG)
+#g.add_node(nH)
+#g.add_node(nI)
+#g.add_node(nJ)
 
 # Debugging
 for node in g.nodeList:
@@ -39,3 +39,5 @@ nD.update_state(1)
 
 for node in g.nodeList:
     print('I AM ' + node.nid + ' AND MY STATE IS: ' + str(node.state))
+
+g.window.getMouse() # pause before closing
